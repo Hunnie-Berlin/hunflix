7cdc77cb2bd9c835df11671635dc53d475b3ae6f
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-10-29 20:48:26 +0100

    TVContainer

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { tvApi } from "../../api";
 import TVPresenter from "./TVPresenter";
 
 class TVContainer extends React.Component {
@@ -9,6 +10,33 @@ class TVContainer extends React.Component {
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      this.setState({
+        topRated,
+        popular,
+        airingToday,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find TV information.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
 
   render() {
     const { topRated, popular, airingToday, error, loading } = this.state;
