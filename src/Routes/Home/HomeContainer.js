c56d65eda292ae7472b00bbbbe1d09d18b8e2e95
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-10-29 11:22:31 +0100

    create structure by "Container Presenter Pattern"

---
+++ src/Routes/Home/HomeContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+
+class HomeContainer extends React.Component {
+  state = {
+    nowPlaying: null,
+    upComing: null,
+    popular: null,
+    error: null,
+    loading: true,
+  };
+  render() {
+    const { nowPlaying, upComing, popular, error, loading } = this.state;
+    return (
+      <HomePresenter
+        nowPlaying={nowPlaying}
+        upComing={upComing}
+        popular={popular}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
+}
+
+export default HomeContainer;
